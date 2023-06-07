f83dd9420d5fba23c268eaff2eb2ede080768ffa
Oladira <106935842+Oladira@users.noreply.github.com>
2023-06-07 14:24:54 +0100

    Add files via upload

---
+++ UserInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserInput = (props) => {
+  return (
+    <div className='input'>
+      <h1>User-Input</h1>
+      <input type="text"  />
+    </div>
+  )
+}
+
+export default UserInput
